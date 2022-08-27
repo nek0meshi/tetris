0ae0ac9195743bd58f6ad81064b78de760f60fcd
nek0meshi <nekmeshi@gmail.com>
2022-08-27 18:45:09 +0900

    make logic independent using hooks

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,22 +1,13 @@
 import Tile from './Tile';
 import './Board.scss';
-import { Block, COLOR_NAME, findBlock } from '../features/blocks';
+import { COLOR_NAME, findBlock } from '../features/blocks';
+import useBlocks from '../hooks/useBlocks';
 
 const BOARD_WIDTH = 10;
 const BOARD_HEIGHT = 25;
 
-// TODO: fix. 現在はダミーデータ.
-const blocks: Block[] = [
-  { x: 1, y: 1, type: 'i' },
-  { x: 1, y: 3, type: 'o' },
-  { x: 4, y: 0, type: 's' },
-  { x: 3, y: 2, type: 'z' },
-  { x: 7, y: 0, type: 'j' },
-  { x: 5, y: 3, type: 'l' },
-  { x: 7, y: 2, type: 't' },
-];
-
 function Board() {
+  const { blocks } = useBlocks();
   const createTile = (x: number, y: number) => {
     const block = findBlock(blocks, x, y);
     const classNames = [block ? COLOR_NAME[block.type] : ''];
