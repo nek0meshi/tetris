8b7c744d863d93c9e774ae29be095116b280843c
nek0meshi <nekmeshi@gmail.com>
2022-08-28 14:34:39 +0900

    move block

--- frontend/src/features/blocks/index.test.ts
+++ frontend/src/features/blocks/index.test.ts
@@ -23,3 +23,43 @@ test('turnOnce', () => {
     ]);
   }
 });
+
+test('turn', () => {
+  const dataSet: [
+    number,
+    number,
+    number,
+    number,
+    block.Turn,
+    number,
+    number
+  ][] = [
+    // turnが0の場合
+    [1, 1, 0, 0, 0, 1, 1],
+    [-1, -1, 0, 0, 0, -1, -1],
+
+    // turnが1の場合
+    [1, 1, 0, 0, 1, 1, -1],
+    [-1, -1, 0, 0, 1, -1, 1],
+    [2, -1, 0, 0, 1, -1, -2],
+    [-1, 3, 0, 0, 1, 3, 1],
+    [4, 4, 3, 3, 1, 4, 2],
+    [2, 2, 3, 3, 1, 2, 4],
+
+    // turnが2の場合
+    [1, 1, 0, 0, 2, -1, -1],
+    [-1, -1, 0, 0, 2, 1, 1],
+    [4, 4, 3, 3, 2, 2, 2],
+    [2, 2, 3, 3, 2, 4, 4],
+
+    // turnが3の場合
+    [1, 1, 0, 0, 3, -1, 1],
+  ];
+
+  for (const [x, y, centerX, centerY, turn, resultX, resultY] of dataSet) {
+    expect(block.turn(x, y, centerX, centerY, turn)).toStrictEqual([
+      resultX,
+      resultY,
+    ]);
+  }
+});
--- frontend/src/hooks/useBlocks.ts
+++ frontend/src/hooks/useBlocks.ts
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import { Block, getNextBlock } from '../features/blocks';
+import { Block, MoveType, getNextBlock, moveBlock } from '../features/blocks';
 
 const DUMMY_BLOCKS: Block[] = [
-  { x: 1, y: 1, type: 'i' },
-  { x: 1, y: 3, type: 'o' },
-  { x: 4, y: 0, type: 's' },
-  { x: 3, y: 2, type: 'z' },
-  { x: 7, y: 0, type: 'j' },
-  { x: 5, y: 3, type: 'l' },
-  { x: 7, y: 2, type: 't' },
+  { x: 1, y: 1, turn: 0, type: 'i' },
+  { x: 1, y: 3, turn: 0, type: 'o' },
+  { x: 4, y: 0, turn: 0, type: 's' },
+  { x: 3, y: 2, turn: 0, type: 'z' },
+  { x: 7, y: 0, turn: 0, type: 'j' },
+  { x: 5, y: 3, turn: 0, type: 'l' },
+  { x: 7, y: 2, turn: 0, type: 't' },
 ];
 
 const useBlocks = (boardWidth: number, boardHeight: number) => {
@@ -19,10 +19,19 @@ const useBlocks = (boardWidth: number, boardHeight: number) => {
     setFallingBlock(getNextBlock(fallingBlock, boardWidth, boardHeight));
   };
 
+  const move = (m: MoveType) => {
+    if (fallingBlock === null) {
+      return;
+    }
+
+    setFallingBlock(moveBlock(fallingBlock, m, boardWidth) || fallingBlock);
+  };
+
   return {
     blocks,
     fallingBlock,
     nextStep,
+    move,
   };
 };
 
