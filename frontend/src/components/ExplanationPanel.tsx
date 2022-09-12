7eb99d43bc328d2f9c04dbbb401a5ab0feb6fcb9
nek0meshi <nekmeshi@gmail.com>
2022-09-12 23:49:06 +0900

    - 説明パネルの追加

---
+++ frontend/src/components/ExplanationPanel.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExplanationPanel.scss';
+
+function ExplanationPanel() {
+  const keyGuideList = [
+    ['←, h', '左移動'],
+    ['→, l', '右移動'],
+    ['↑, k', '回転'],
+    ['↓, j', '下移動'],
+    ['Space', '下まで落下'],
+  ].map(([key, text], i) => (
+    <dl key={i}>
+      <dt>{key}</dt>
+      <dd>{text}</dd>
+    </dl>
+  ));
+  return (
+    <aside className="ExplanationPanel">
+      <h3 className="subtitle">キー操作</h3>
+      <dl>{keyGuideList}</dl>
+    </aside>
+  );
+}
+
+export default ExplanationPanel;
