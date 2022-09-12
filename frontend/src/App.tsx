f620717aff22a126860609498368e73296cee2c7
nek0meshi <nekmeshi@gmail.com>
2022-09-12 23:49:06 +0900

    - 説明パネルの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Board from './components/Board';
+import ExplanationPanel from './components/ExplanationPanel';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Tetris</h1>
-      <Board />
+      <main className="main">
+        <ExplanationPanel />
+        <Board />
+      </main>
     </div>
   );
 }
