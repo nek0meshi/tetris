b98c65161bfdd8a9ce1615388f39d9dc12335e70
nek0meshi <nekmeshi@gmail.com>
2023-10-01 22:46:27 +0900

    memo化

--- frontend/src/components/GameOver.tsx
+++ frontend/src/components/GameOver.tsx
@@ -1,23 +1,28 @@
-import 'react';
+import { useMemo } from 'react';
 import './GameOver.scss';
 
 function GameOver() {
   // keyに利用している定数について
   //   100, 10000は適当な値.
   //   span, br共通で、重複しない必要がある.
-  const text = ['GAME', 'OVER']
-    .map((item, index1) =>
-      item
-        .split('')
-        .map((t, index2) => <span key={index1 * 100 + index2}>{t}</span>)
-    )
-    .reduce(
-      (prev, cur, index) =>
-        (prev.length > 0 ? [...prev, <br key={10000 + index} />] : []).concat([
-          ...cur,
-        ]),
-      []
-    );
+  const text = useMemo(
+    () =>
+      ['GAME', 'OVER']
+        .map((item, index1) =>
+          item
+            .split('')
+            .map((t, index2) => <span key={index1 * 100 + index2}>{t}</span>)
+        )
+        .reduce(
+          (prev, cur, index) =>
+            (prev.length > 0
+              ? [...prev, <br key={10000 + index} />]
+              : []
+            ).concat([...cur]),
+          []
+        ),
+    []
+  );
 
   return (
     <div className="GameOver">
